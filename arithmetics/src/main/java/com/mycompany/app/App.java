66fb760b3e4adbe9740573f37c0804f80a463802
genxium <genxium@hotmail.com>
2020-04-28 16:44:57 +0800

    A dirty commit.

--- arithmetics/src/main/java/com/mycompany/app/App.java
+++ arithmetics/src/main/java/com/mycompany/app/App.java
@@ -2,20 +2,26 @@ package com.mycompany.app;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
 public class App {
     final private static Object lock = new Object();
 
     public static void main(String[] args) {
-        /**
-         * When invoked from "com.mycompany.hybrid.HybridApp/main" together with "com.mycompany.stringhelper.app.App/main",
-         * the following invocation by "Spring ApplicationContext" would fail due to contamination of '<bean id="lib" />'.
-         *
-         // final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-         // final Lib libIns = (Lib) ctx.getBean("lib");
-         // ctx.refresh();
-         // ctx.start();
-         */
-        final Lib libIns = new Lib();
+        final InputStream inputStream = App.class.getResourceAsStream("/applicationContext.xml");
+        final InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        final String content = new BufferedReader(streamReader).lines().collect(Collectors.joining("\n"));
+        System.out.println("Hello arithmetics! The content of `applicationContext.xml` is \n" + content);
+
+        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ctx.refresh();
+        ctx.start();
+        System.out.println("Hello arithmetics! ctx.getId() is " + ctx.getId() + ", ctx.getBeanFactory().hashCode() is " + ctx.getBeanFactory().hashCode());
+
+        final Lib libIns = (Lib) ctx.getBean("lib");
+
         final int plusResult = libIns.plus(1, 2);
         System.out.println("Hello arithmetics! Plus result is " + plusResult);
         synchronized (lock) {
--- stringhelper/src/main/java/com/mycompany/app/App.java
+++ stringhelper/src/main/java/com/mycompany/app/App.java
@@ -1,20 +1,28 @@
 package com.mycompany.app;
 
-public class App
-{
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+public class App {
     final private static Object lock = new Object();
-    public static void main( String[] args )
-    {
-        /**
-         * When invoked from "com.mycompany.hybrid.HybridApp/main" together with "com.mycompany.arithmetics.app.App/main",
-         * the following invocation by "Spring ApplicationContext" would fail due to contamination of '<bean id="lib" />'.
-         *
-         // final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-         // final Lib libIns = (Lib) ctx.getBean("lib");
-         // ctx.refresh();
-         // ctx.start();
-         */
-        final Lib libIns = new Lib();
+
+    public static void main(String[] args) {
+        final InputStream inputStream = App.class.getResourceAsStream("/applicationContext.xml");
+        final InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        final String content = new BufferedReader(streamReader).lines().collect(Collectors.joining("\n"));
+        System.out.println("Hello stringhelper! The content of `applicationContext.xml` is \n" + content);
+
+        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ctx.refresh();
+        ctx.start();
+        System.out.println("Hello stringhelper! ctx.getId() is " + ctx.getId() + ", ctx.getBeanFactory().hashCode() is " + ctx.getBeanFactory().hashCode());
+        final Lib libIns = (Lib) ctx.getBean("lib");
+
         final String reversedStr = libIns.reverse("foobar");
         System.out.println("Hello stringhelper! Reversed string result is " + reversedStr);
         synchronized (lock) {
