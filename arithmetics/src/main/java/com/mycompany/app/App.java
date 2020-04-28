3d5521e1f29ff6a357843f0a8f9f9f4ec687d9e8
genxium <genxium@hotmail.com>
2020-04-28 14:24:18 +0800

    Commented out conflict use of Spring ApplicationContext.

--- arithmetics/src/main/java/com/mycompany/app/App.java
+++ arithmetics/src/main/java/com/mycompany/app/App.java
@@ -6,12 +6,18 @@ public class App {
     final private static Object lock = new Object();
 
     public static void main(String[] args) {
-        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        final Lib libIns = (Lib) ctx.getBean("lib");
+        /**
+         * When invoked from "com.mycompany.hybrid.HybridApp/main" together with "com.mycompany.stringhelper.app.App/main",
+         * the following invocation by "Spring ApplicationContext" would fail due to contamination of '<bean id="lib" />'.
+         *
+         // final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+         // final Lib libIns = (Lib) ctx.getBean("lib");
+         // ctx.refresh();
+         // ctx.start();
+         */
+        final Lib libIns = new Lib();
         final int plusResult = libIns.plus(1, 2);
         System.out.println("Hello arithmetics! Plus result is " + plusResult);
-        ctx.refresh();
-        ctx.start();
         synchronized (lock) {
             try {
                 lock.wait();
--- stringhelper/src/main/java/com/mycompany/app/App.java
+++ stringhelper/src/main/java/com/mycompany/app/App.java
@@ -1,17 +1,22 @@
 package com.mycompany.app;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App
 {
     final private static Object lock = new Object();
     public static void main( String[] args )
     {
-        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        final Lib libIns = (Lib) ctx.getBean("lib");
+        /**
+         * When invoked from "com.mycompany.hybrid.HybridApp/main" together with "com.mycompany.arithmetics.app.App/main",
+         * the following invocation by "Spring ApplicationContext" would fail due to contamination of '<bean id="lib" />'.
+         *
+         // final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+         // final Lib libIns = (Lib) ctx.getBean("lib");
+         // ctx.refresh();
+         // ctx.start();
+         */
+        final Lib libIns = new Lib();
         final String reversedStr = libIns.reverse("foobar");
         System.out.println("Hello stringhelper! Reversed string result is " + reversedStr);
-        ctx.refresh();
-        ctx.start();
         synchronized (lock) {
             try {
                 lock.wait();
